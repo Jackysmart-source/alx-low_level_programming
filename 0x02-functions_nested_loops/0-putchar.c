0c1374263b9d9caee10f051065c794c9715c1b6d
Jacqueline <jacquelineokonko@gmail.com>
2022-09-13 07:45:05 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,8 @@
-#include <unistd.h>
 #include "main.h"
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
 int main(void)
 {
 	_putchar('_');
@@ -10,6 +13,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
